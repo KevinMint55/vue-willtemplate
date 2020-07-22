70c1cdc9c230f386c47dbf766612793415cd323c
KevinMint55 <innovation55@foxmail.com>
2020-07-22 17:44:08 +0800

    init

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const state = {
+  name: 'cli',
+};
+
+export default new Vuex.Store({
+  state,
+  getters: {},
+  actions: {},
+  mutations: {},
+});
---
+++ src/views/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2 :class="s.title">vue</h2>
+    <img src="../assets/me.jpg" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  mounted() {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="less" module="s">
+.title {
+  display: flex;
+  color: green;
+}
+</style>
